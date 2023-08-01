0a76a80909318f7fc4b5bc643246a9c2b96d0054
Vesa6 <83303692+Vesa6@users.noreply.github.com>
2023-08-01 22:56:27 +0300

    part 5 final

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,60 +1,114 @@
+//Makes a new router object.
 const blogsRouter = require('express').Router()
-const Note = require('../models/note')
+const Blog = require('../models/blog')
+const userWare = require('../utils/middleware').userWare
 
-blogsRouter.get('/', (request, response) => {
-  Blog.find({}).then(blogs => {
-    response.json(blogs)
-  })
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
 })
 
-blogsRouter.get('/:id', (request, response, next) => {
-  Blog.findById(request.params.id)
-    .then(blog => {
-      if (blog) {
-        response.json(blog)
-      } else {
-        response.status(404).end()
-      }
-    })
-    .catch(error => next(error))
+blogsRouter.get('/:id', async (request, response, next) => {
+  try {
+    //TODO: Ensure that blog always has ID no matter what
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.post('/', (request, response, next) => {
+// Not needed with middleware
+// Preserve for reference
+
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.startsWith('Bearer ')) {
+//     return authorization.replace('Bearer ', '')
+//   }
+//   return null
+// }
+
+blogsRouter.post('/', userWare, async (request, response, next) => {
   const body = request.body
 
+  //User is given by the middleware
+  const user = request.user
+  if (!user) {
+    return response.status(401).json({ error: 'token invalid' })
+  }
+
+  if (!body.title || !body.url) {
+    const respondStatus = response.status(400).json({ error: 'no title or url' })
+    return respondStatus
+  }
+
   const blog = new Blog({
-    content: body.content,
-    important: body.important || false,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0,
+    user: user._id
   })
 
-  blog.save()
-    .then(savedBlog => {
-      response.json(savedBlog)
-    })
-    .catch(error => next(error))
+  try {
+    const saved = await blog.save()
+    user.blogs = user.blogs.concat(saved._id)
+    await user.save()
+    response.status(201).json(saved.toJSON())
+  // All errors are passed to middleware
+  } catch (error) {
+    next(error)
+  }
 })
 
-blogsRouter.delete('/:id', (request, response, next) => {
-  Blog.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
-})
+blogsRouter.delete('/:id', userWare, async (request, response, next) => {
 
-blogsRouter.put('/:id', (request, response, next) => {
+  try {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch(error){
+    next(error)
+  }
+}
+)
+
+//   Blog.findByIdAndRemove(request.params.id)
+//     .then(() => {
+//       response.status(204).end()
+//     })
+//     .catch(error => next(error))
+// })
+
+blogsRouter.put('/:id', userWare, async (request, response, next) => {
   const body = request.body
 
   const blog = {
-    content: body.content,
-    important: body.important,
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
   }
 
-  Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
-    .then(updatedBlog => {
-      response.json(updatedBlog)
-    })
-    .catch(error => next(error))
+  try {
+    const update = await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+    response.json(update)
+  } catch (error) {
+    next(error)
+  }
 })
+
+//   }
+//   Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+//     .then(updatedBlog => {
+//       response.json(updatedBlog)
+//     })
+//     .catch(error => next(error))
+// })
 
 module.exports = blogsRouter
